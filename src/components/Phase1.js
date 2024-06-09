964ff4370e4a4049d270bf8ba0cb7b5f74f77da7
Anilkokkul <anilkokkul1997@gmail.com>
2024-06-09 12:39:06 +0530

    updated

--- src/components/Phase1.js
+++ src/components/Phase1.js
@@ -1,18 +1,96 @@
-import React from 'react'
+import React from "react";
 
 const Phase1 = () => {
   return (
-    <div className=' text-center p-5'>
-        <h1 className=" text-5xl my-10 text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500">
+    <div className="  p-5">
+      <h1 className="text-center text-5xl my-10 text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-fuchsia-500">
         Roadmap
       </h1>
-      <div>
-        <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-sky-500 text-3xl font-semibold'>
+      <div className=" flex p-10 justify-center items-center">
+        <div className=" w-[70%] p-20">
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-r ml-10 from-[#faca6d] to-[#fa78f2] text-5xl font-semibold">
             PHASE 1
-        </h1>
+          </h1>
+          <ul className=" list-disc text-left text-xl ml-20  mt-10 ">
+            <li className="leading-7">
+              Horizen dai dai harmony dogecoin waves nexo.
+            </li>
+            <li className="leading-7">
+              Flow horizen waves dash tether zcash waves dash terraUSD.
+            </li>
+            <li className="leading-7">
+              Quant harmony amp cosmos PancakeSwap decentraland decred.
+            </li>
+            <li className="leading-7">
+              Serum TRON solana tether holo crypto. Flow eCash amp EOS digibyte
+              stellar.
+            </li>
+          </ul>
+        </div>
+        <div className="w-[40%] h-[70vh]">
+          <img
+            src="/phase1.png"
+            alt="phase1"
+            className=" relative right-[-20%] h-full w-full rounded-2xl"
+          />
+        </div>
+      </div>
+      <div className=" flex p-10 justify-center items-center">
+        <div className=" w-[70%] p-20">
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-r ml-10 from-[#faca6d] to-[#fa78f2] text-5xl font-semibold">
+            PHASE 2
+          </h1>
+          <ul className=" list-disc text-left text-xl ml-20  mt-10 ">
+            <li className="leading-7">
+              BitTorrent dai dogecoin crypto nexo nexo cosmos tezos ox
+              PancakeSwap.
+            </li>
+            <li className="leading-7">
+              Tether ethereum helium eCash IOTA. Litecoin enjin shiba-inu dash
+              audius monero.
+            </li>
+            <li className="leading-7">
+              Aave PancakeSwap loopring horizen neo uniswap avalanche litecoin
+              celsius TRON.
+            </li>
+            <li className="leading-7">Hive stellar velas flow cardano.</li>
+          </ul>
+        </div>
+        <div className="w-[40%] h-[70vh]">
+          <img
+            src="/phase2.png"
+            alt="phase3"
+            className=" relative right-[-15%] h-full w-full rounded-2xl"
+          />
+        </div>
+      </div>
+      <div className=" flex p-10 justify-center items-center">
+        <div className=" w-[70%] p-20">
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-r ml-10 from-[#faca6d] to-[#fa78f2] text-5xl font-semibold">
+            PHASE 3
+          </h1>
+          <ul className=" list-disc text-left text-xl ml-20  mt-10 ">
+            <li className="leading-7">
+              Revain audius bitcoin revain PancakeSwap elrond ipsum ethereum.
+            </li>
+            <li className="leading-7">
+              Waves TRON dogecoin bancor eCash quant secret.
+            </li>
+            <li className="leading-7">
+              Celsius crypto bancor nexo litecoin decentraland crypto.
+            </li>
+          </ul>
+        </div>
+        <div className="w-[40%] h-[70vh]">
+          <img
+            src="/phase3.png"
+            alt="phase3"
+            className=" relative right-[-20%] h-full w-full rounded-2xl"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Phase1
+export default Phase1;
--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -10,24 +10,24 @@ const Tokenomics = () => {
         <div className=" grid grid-cols-[1fr,10px,1fr] gap-1 text-left">
             <p className="font-semibold">Name</p>
             <p>:</p>
-            <p className="ml-4">AI GEN</p>
+            <p className="ml-4 text-slate-300">AI GEN</p>
             <p className="font-semibold">Total Supply</p>
             <p>:</p>
-            <p className="ml-4"> 1B</p>
+            <p className="ml-4 text-slate-300"> 1B</p>
             <p className="font-semibold">Contract</p>
             <p>:</p>
-            <p className="ml-4"> Lorem ipsum dolor sit a...</p>
+            <p className="ml-4 text-slate-300"> Lorem ipsum dolor sit a...</p>
         </div>
         <div className=" grid grid-cols-[1fr,10px,1fr] gap-1 text-left">
             <p className="font-semibold">Token Name</p>
             <p >:</p>
-            <p className="ml-4">$AIGEN</p>
+            <p className="ml-4 text-slate-300">$AIGEN</p>
             <p className="font-semibold">Circulating Supply</p>
             <p>:</p>
-            <p className="ml-4">1B</p>
+            <p className="ml-4 text-slate-300">1B</p>
             <p className="font-semibold">Chain</p>
             <p >:</p>
-            <p className="ml-4">Ethereum</p>
+            <p className="ml-4 text-slate-300">Ethereum</p>
         </div>
       </div>
       <div className=" flex justify-center items-center p-5 gap-8 mt-5">
