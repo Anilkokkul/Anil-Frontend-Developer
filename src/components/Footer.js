87eb82edbefb7ed089511e2b5239d3dc51c64a59
Anilkokkul <anilkokkul1997@gmail.com>
2024-06-11 16:52:21 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
-import { motion } from "framer-motion";
+import { FaTiktok } from "react-icons/fa";
 import React from "react";
+import { FaYoutube } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="">
-      <div className="flex gap-16 justify-center items-start h-64 overflow-hidden">
+    <div className="px-10">
+      <div className="flex gap-16 justify-center items-start h-64 overflow-hidden relative">
         <div whileInView={{ y: "-100%" }} className=" flex flex-col gap-8 ">
           <img src="/7-1.png" alt="pic-1" className=" w-36 h-36 rounded-lg" />
           <img
@@ -118,6 +122,39 @@ const Footer = () => {
             alt="pic-3"
             className=" w-36 h-36 rounded-lg object-cover"
           />
+        </div>
+        <div className=" w-[80%] flex justify-between cursor-pointer items-center rounded-lg absolute bg-gradient-to-r from-[#8158b8] to-[#c85ec6] h-24 bottom-0 ">
+          <h1 className=" text-5xl ml-12">Get Started</h1>
+          <FaArrowRight className=" text-5xl mr-12" />
+        </div>
+      </div>
+      <div className="flex justify-between items-center py-20">
+        <div className=" w-[350px] text-xl">
+          Dash dash terraUSD neo uniswap kadena helium avalanche polymath
+          bancor.
+        </div>
+        <div className=" text-right">
+          <div>Looking for help?</div>
+          <div className=" mt-2">
+            <input
+              type="text"
+              className=" border-2 bg-transparent rounded-md h-10 p-2"
+              placeholder="Enter your email"
+            />{" "}
+            <button className="bg-gradient-to-r px-4 py-2 h-10 ml-1 rounded-md from-[#8158b8] to-[#c85ec6]">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <div className=" flex justify-between items-center py-5">
+        <div className=" text-xl tracking-widest">AI. GEN</div>
+        <div className=" text-white flex gap-2">
+          <FaTwitter />
+          <FaLinkedin />
+          <FaTiktok />
+          <FaYoutube />
         </div>
       </div>
     </div>
