2ccbb4e97a55d572ca71bb8e257efae3e147ca8d
Anilkokkul <anilkokkul1997@gmail.com>
2024-06-12 12:40:25 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,130 +6,134 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="px-10">
-      <div className="flex gap-16 justify-center items-start h-64 overflow-hidden relative">
-        <div whileInView={{ y: "-100%" }} className=" flex flex-col gap-8 ">
-          <img src="/7-1.png" alt="pic-1" className=" w-36 h-36 rounded-lg" />
+    <div className="lg:px-10 px-5">
+      <div className="lg:py-20 flex relative lg:gap-20 gap-5 w-full justify-center items-center lg:h-64 h-32 overflow-hidden">
+        <div className=" flex flex-col lg:gap-8 gap-5 ">
+          <img
+            src="/7-1.png"
+            alt="pic-1"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg"
+          />
           <img
             src="/7-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/7-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover relative"
+            className="lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" flex flex-col gap-8 relative top-[-70px]">
+        <div className=" flex flex-col lg:gap-8 gap-5 relative lg:top-[-70px] top-[-30px]">
           <img
             src="/6-1.png"
             alt="pic-1"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-10 h-16  rounded-lg object-cover"
           />
           <img
             src="/6-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/6-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" flex flex-col gap-8 ">
+        <div className=" flex flex-col lg:gap-8 gap-5 ">
           <img
             src="/5-1.png"
             alt="pic-1"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/5-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16  rounded-lg object-cover"
           />
           <img
             src="/5-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" flex flex-col gap-8 relative top-[-70px]">
+        <div className=" flex flex-col lg:gap-8 gap-5 relative lg:top-[-70px] top-[-30px]">
           <img
             src="/4-1.png"
             alt="pic-1"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/4-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36  lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/4-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" flex flex-col gap-8 ">
+        <div className=" flex flex-col lg:gap-8 gap-5">
           <img
             src="/3-1.png"
             alt="pic-1"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/3-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/3-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" flex flex-col gap-8 relative top-[-70px]">
+        <div className=" flex flex-col lg:gap-8 gap-5 relative lg:top-[-70px] top-[-30px]">
           <img
             src="/2-1.png"
             alt="pic-1"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/2-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/2-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" flex flex-col gap-8 ">
+        <div className=" flex flex-col lg:gap-8 gap-5 ">
           <img
             src="/nutral-pic.png"
             alt="pic-1"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/nutral-pic-2.png"
             alt="pic-2"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
           <img
             src="/nutral-pic-3.png"
             alt="pic-3"
-            className=" w-36 h-36 rounded-lg object-cover"
+            className=" lg:w-36 lg:h-36 w-16 h-16 rounded-lg object-cover"
           />
         </div>
-        <div className=" w-[80%] flex justify-between cursor-pointer items-center rounded-lg absolute bg-gradient-to-r from-[#8158b8] to-[#c85ec6] h-24 bottom-0 ">
-          <h1 className=" text-5xl ml-12">Get Started</h1>
-          <FaArrowRight className=" text-5xl mr-12" />
+        <div className=" w-[80%] flex justify-between cursor-pointer items-center rounded-lg absolute bg-gradient-to-r from-[#8158b8] to-[#c85ec6] lg:h-24 h-12 bottom-0 ">
+          <h1 className=" lg:text-5xl text-xl ml-[5%]">Get Started</h1>
+          <FaArrowRight className=" lg:text-5xl text-xl mr-[5%]" />
         </div>
       </div>
       <div className="flex justify-between items-center py-20">
-        <div className=" w-[350px] text-xl">
+        <div className=" w-[350px] lg:text-xl text-sm">
           Dash dash terraUSD neo uniswap kadena helium avalanche polymath
           bancor.
         </div>
@@ -138,7 +142,7 @@ const Footer = () => {
           <div className=" mt-2">
             <input
               type="text"
-              className=" border-2 bg-transparent rounded-md h-10 p-2"
+              className=" border-2 bg-transparent rounded-md lg:h-10 h-5 w-44 p-2"
               placeholder="Enter your email"
             />{" "}
             <button className="bg-gradient-to-r px-4 py-2 h-10 ml-1 rounded-md from-[#8158b8] to-[#c85ec6]">
@@ -147,7 +151,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <hr/>
+      <hr />
       <div className=" flex justify-between items-center py-5">
         <div className=" text-xl tracking-widest">AI. GEN</div>
         <div className=" text-white flex gap-2">
